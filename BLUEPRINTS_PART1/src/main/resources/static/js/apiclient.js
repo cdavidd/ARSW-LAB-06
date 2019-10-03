03831f92fa531dab33c36ec900d3a56cce3e9b96
david <cdavidlopez@hotmail.com>
2019-10-03 11:08:57 -0500

    promises, -tabla, -errores

--- BLUEPRINTS_PART1/src/main/resources/static/js/apiclient.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/apiclient.js
@@ -2,37 +2,73 @@ const apiUrl = "http://localhost:8080/blueprints/";
 apiclient = (function() {
   return {
     getBlueprintsByAuthor: function(name, callback) {
-      jQuery.ajax({
+      var getPromise = jQuery.ajax({
         url: apiUrl + name,
         success: function(result) {
           callback(result);
         },
         async: true
       });
+      getPromise.then(
+        function() {
+          console.info("OK getBlueprintsByAuthor");
+        },
+        function() {
+          alert("No se encuentra el author: " + name);
+        }
+      );
     },
     getBlueprintsByNameAndAuthor: function(author, name, callback) {
-      jQuery.ajax({
+      var getPromise = jQuery.ajax({
         url: apiUrl + author + "/" + name,
         success: function(result) {
           callback(result);
         },
         async: true
       });
+      getPromise.then(
+        function() {
+          console.info("OK getBlueprintsByNameAndAuthor");
+        },
+        function() {
+          alert(
+            "No se encuentra el author: " + author + " con el plano: " + name
+          );
+        }
+      );
     },
     setBlueprint: function(author, name, newBp) {
-      $.ajax({
+      var putPromise = $.ajax({
         url: "/blueprints/" + author + "/" + name + "/",
         type: "PUT",
         data: newBp,
         contentType: "application/json"
       });
+
+      putPromise.then(
+        function() {
+          console.info("OK setBlueprint");
+        },
+        function() {
+          console.info("ERROR setBlueprint");
+        }
+      );
     },
     deletePlane: function(author, name) {
-      $.ajax({
+      var delPromise = $.ajax({
         url: "/blueprints/" + author + "/" + name + "/",
         type: "DELETE",
         contentType: "application/json"
       });
+
+      delPromise.then(
+        function() {
+          console.info("OK deletePlane");
+        },
+        function() {
+          console.info("ERROR deletePlane");
+        }
+      );
     }
   };
 })();
