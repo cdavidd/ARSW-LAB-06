f9861faf56bb5976b8735d06db42bde8011369ac
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-10-03 19:50:38 -0500

    API test

--- BLUEPRINTS_PART1/src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
+++ BLUEPRINTS_PART1/src/test/java/edu/eci/arsw/blueprints/test/services/ApplicationServicesTests.java
@@ -1,20 +1,95 @@
 package edu.eci.arsw.blueprints.test.services;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprintsapi.BlueprintsAPIApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes = BlueprintsAPIApplication.class)
+@WebAppConfiguration
 public class ApplicationServicesTests {
-    
+
+    protected MockMvc mvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
-    public void contextLoads() { 
+    public void getBlueprintsTest() throws Exception {
+        String uri = "/blueprints";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(202, status);
+    }
+
+    @Test
+    public void createBlueprintTest() throws Exception {
+        String uri = "/blueprints";
+        Blueprint product = new Blueprint("test author","test b",new Point[]{new Point(1,1)});
+
+        String inputJson = mapToJson(product);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+    }
+
+    @Test
+    public void updateProduct() throws Exception {
+        String uri = "/blueprints/author1/Blueprint_a";
+        Blueprint blueprint = new Blueprint();
+        String inputJson = mapToJson(blueprint);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(201, status);
+    }
+
+    @Test
+    public void deleteBlueprintTest() throws Exception {
+        String uri = "/blueprints/author1/Blueprint_a";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(202, status);
     }
 
 }
